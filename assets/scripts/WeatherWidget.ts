fd8cec157dc1e2f22259ec3e6ef998195e8189a0
long2805l2 <long2805l2@gmail.com>
2021-05-19 11:49:33 +0700

    -try parse data

--- assets/scripts/WeatherWidget.ts
+++ assets/scripts/WeatherWidget.ts
@@ -6,36 +6,57 @@ const { ccclass, property } = _decorator;
 @ccclass('WeatherWidget')
 export class WeatherWidget extends Component
 {
+    @property
+	country:string = "London";
+	
+	private _temperature:number = 0;
+	private _feelslike:number = 0;
+	private _humidity:number = 0;
+	private _wind:number = 0;
+	
 	start ()
 	{
-		let api_url = WeatherAPI.API_URL_GET_BY_CITY ("London");
-		let data = WeatherAPI.GET_DATA (api_url,
-			(error:Error, jsonStr:string) => {
-				if (error)
-				{
-					log ("error", error.name, error.message);
-					return;
-				}
-
-				if (jsonStr)
-				{
-					let obj = JSON.parse (jsonStr);
-					log ("data", obj);
-					return;
-				}
-			}
+		OpenWeather.GET_DATA (
+			OpenWeather.API_URL_GET_BY_CITY (this.country),
+			this.onLoadCompleted.bind (this)
 		);
 	}
-	
-}
 
-/**
- * [1] Class member could be defined like this.
- * [2] Use `property` decorator if your want the member to be serializable.
- * [3] Your initialization goes here.
- * [4] Your update function goes here.
- *
- * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
- * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
- * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
- */
+	onLoadCompleted (error:Error, jsonStr:string)
+	{
+		if (error)
+		{
+			log ("onLoadCompleted", "error", error.name, error.message);
+			return;
+		}
+
+		if (jsonStr)
+			this.parse (jsonStr);
+	}
+	
+	parse (weatherJson:string)
+	{
+		if (!weatherJson)
+			return;
+
+		let obj = null;
+		try
+		{
+			obj = JSON.parse (weatherJson);
+			this._temperature = obj.main.temp;
+			this._feelslike = obj.main.feels_like;
+			this._humidity = obj.main.humidity;
+			this._wind = obj.wind.speed;
+		}
+		catch (error)
+		{
+			log ("parse error", error);
+			return;
+		}
+
+		log ("temperature", this._temperature);
+		log ("feelslike", this._feelslike);
+		log ("humidity", this._humidity);
+		log ("wind", this._wind);
+	}
+}
