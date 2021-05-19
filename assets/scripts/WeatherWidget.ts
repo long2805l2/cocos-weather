844246d8e66e65281ffa0f32c173117807a7c407
long2805l2 <long2805l2@gmail.com>
2021-05-19 10:49:03 +0700

    +add weather widget

---
+++ assets/scripts/WeatherWidget.ts
@@ -0,0 +1,41 @@
+
+import { _decorator, Component, Node, log } from 'cc';
+import { WeatherAPI } from './WeatherAPI';
+const { ccclass, property } = _decorator;
+
+@ccclass('WeatherWidget')
+export class WeatherWidget extends Component
+{
+	start ()
+	{
+		let api_url = WeatherAPI.API_URL_GET_BY_CITY ("London");
+		let data = WeatherAPI.GET_DATA (api_url,
+			(error:Error, jsonStr:string) => {
+				if (error)
+				{
+					log ("error", error.name, error.message);
+					return;
+				}
+
+				if (jsonStr)
+				{
+					let obj = JSON.parse (jsonStr);
+					log ("data", obj);
+					return;
+				}
+			}
+		);
+	}
+	
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
+ */
