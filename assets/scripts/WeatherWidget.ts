c863153d7e2bf7071e90fe08fb05986e8b32131a
long2805l2 <long2805l2@gmail.com>
2021-05-19 15:29:55 +0700

    +add scene to show weather detail

--- assets/scripts/WeatherWidget.ts
+++ assets/scripts/WeatherWidget.ts
@@ -1,23 +1,43 @@
 
-import { _decorator, Component, Node, log } from 'cc';
+import { _decorator, Component, Node, log, CCLoader, Label, RichText, Gradient, Graphics, SpriteFrame, Sprite, resources, Texture2D, assetManager, ImageAsset } from 'cc';
 import { OpenWeather } from './OpenWeather';
 const { ccclass, property } = _decorator;
 
 @ccclass('WeatherWidget')
 export class WeatherWidget extends Component
 {
-    @property
-	country:string = "London";
+	@property
+	public cityName:string = "London";
+
+	@property({type: RichText})
+	public city: RichText|null = null;
+
+	@property({type: RichText})
+	public temperature: RichText|null = null;
+
+	@property({type: RichText})
+	public feelslike: RichText|null = null;
+
+	@property({type: RichText})
+	public humidity: RichText|null = null;
+
+	@property({type: RichText})
+	public wind: RichText|null = null;
+
+	@property({type: Sprite})
+	public weatherIcon: Sprite|null = null;
 	
+	private _data:any = null;
 	private _temperature:number = 0;
 	private _feelslike:number = 0;
 	private _humidity:number = 0;
 	private _wind:number = 0;
+	private _weatherIcon:string = "";
 	
 	start ()
 	{
 		OpenWeather.GET_DATA (
-			OpenWeather.API_URL_GET_BY_CITY (this.country),
+			OpenWeather.API_URL_GET_BY_CITY (this.cityName),
 			this.onLoadCompleted.bind (this)
 		);
 	}
@@ -32,6 +52,8 @@ export class WeatherWidget extends Component
 
 		if (jsonStr)
 			this.parse (jsonStr);
+
+		this.fill ();
 	}
 	
 	parse (weatherJson:string)
@@ -43,20 +65,65 @@ export class WeatherWidget extends Component
 		try
 		{
 			obj = JSON.parse (weatherJson);
+			this._data = obj;
 			this._temperature = obj.main.temp;
 			this._feelslike = obj.main.feels_like;
 			this._humidity = obj.main.humidity;
 			this._wind = obj.wind.speed;
+			this._weatherIcon = OpenWeather.ICON_WEATHER_URL (obj.weather [0].icon);
 		}
 		catch (error)
 		{
 			log ("parse error", error);
 			return;
 		}
+	}
 
-		log ("temperature", this._temperature);
-		log ("feelslike", this._feelslike);
-		log ("humidity", this._humidity);
-		log ("wind", this._wind);
+	fill ()
+	{
+		if (!this._data)
+			return;
+
+		log ("data", JSON.stringify (this._data));
+
+		if (this.city)
+		{
+			if (this._data)
+				this.city.string = this._data.name;
+			else
+				this.city.string = this.cityName;
+		}
+
+		if (this.temperature)
+			this.temperature.string = `${this._temperature} °C`;
+
+		if (this.feelslike)
+			this.feelslike.string = `${this._feelslike} °C`;
+
+		if (this.humidity)
+			this.humidity.string = `${this._humidity} %`;
+			
+		if (this.wind)
+			this.wind.string = `${this._wind} m/s`;
+
+		if (this.weatherIcon)
+		{
+			let icon = this.weatherIcon;
+			log ("this._weatherIcon", this._weatherIcon);
+			assetManager.loadRemote(`${this._weatherIcon}`, (err: any, texture: ImageAsset) =>
+			{
+				if (err)
+				{
+					log ("fill", "icon", "error", err);
+					return;
+				}
+				if (!texture)
+					return;
+					
+				const spriteFrame = new SpriteFrame();
+				spriteFrame.texture = texture._texture;
+				icon.spriteFrame = spriteFrame;
+			});
+		}
 	}
 }
