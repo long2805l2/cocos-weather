0bfbc2ee3d466c9832b93b10bb036eccaa87dc24
long2805l2 <long2805l2@gmail.com>
2021-05-22 17:44:28 +0700

    -app activity: format src, update get current location

--- native/engine/android/app/src/com/cocos/game/AppActivity.java
+++ native/engine/android/app/src/com/cocos/game/AppActivity.java
@@ -27,167 +27,186 @@ public class AppActivity extends CocosActivity
 	private static AppActivity app = null;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-		if (!isTaskRoot()) {
-			return;
-		}
-		SDKWrapper.shared().init(this);
+		
 		app = this;
+
+		if (!isTaskRoot())
+			return;
+		
+		SDKWrapper.shared().init(this);
 	}
 
 	private static double lastLatitude = 0;
 	private static double lastLongitude = 0;
 	
 	@Override
-	public void onLocationChanged(Location location) {
+	public void onLocationChanged(Location location)
+	{
 		double newLatitude = location.getLatitude();
 		double newLongitude = location.getLongitude();
-		System.out.println("com.weather.AppActivity::onLocationChanged, check " + newLatitude + ", " + newLongitude);
-		if (lastLatitude == 0 && lastLongitude == 0)
+		
+		System.out.println("com.weather.AppActivity::onLocationChanged " + newLatitude + ", " + newLongitude);
+		
+		if (newLatitude != 0 && newLongitude != 0)
 		{
-			CocosHelper.runOnGameThread(new Runnable() {
-				@Override
-				public void run() {
-					System.out.println("com.weather.AppActivity::onLocationChanged, runOnGameThread " + newLatitude + ", " + newLongitude);
-				}
-			});
+			lastLatitude = newLatitude;
+			lastLongitude = newLongitude;
 		}
-		lastLatitude = newLatitude;
-		lastLongitude = newLongitude;
 	}
 
 	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		String msg="com.myweather.AppActivity, onStatusChanged: " + provider + ", " + status;
-		System.out.println (msg);
+	public void onStatusChanged(String provider, int status, Bundle extras)
+	{
+		System.out.println ("com.myweather.AppActivity, onStatusChanged: " + provider + ", " + status);
 	}
 
 	@Override
-	public void onProviderEnabled(String provider) {
-		String msg="com.myweather.AppActivity, onProviderEnabled: " + provider;
-		System.out.println (msg);
+	public void onProviderEnabled(String provider)
+	{
+		System.out.println ("com.myweather.AppActivity, onProviderEnabled: " + provider);
 	}
 
 	@Override
-	public void onProviderDisabled(String provider) {
-		String msg="com.myweather.AppActivity, onProviderDisabled: " + provider;
-		System.out.println (msg);
+	public void onProviderDisabled(String provider)
+	{
+		System.out.println ("com.myweather.AppActivity, onProviderDisabled: " + provider);
+	}
+	
+	public static String getCurrentLocation ()
+	{
+		if (lastLatitude != 0 && lastLongitude != 0)
+			return "{\"lat\": " + lastLatitude + ", \"lon\": " + lastLongitude + "}";
+
+		getLocationCoordinate ();
+		return "";
 	}
 
-	public static void getLocationCoordinate() {
+	public static void getLocationCoordinate ()
+	{
 		System.out.println("com.weather.AppActivity::getLocationCoordinate check " + lastLatitude + "," + lastLongitude);
 		if (lastLatitude != 0 && lastLongitude != 0)
 			return;
 		
-		app.runOnUiThread(new Runnable() {
-			@TargetApi(Build.VERSION_CODES.M)
+		app.runOnUiThread (new Runnable ()
+		{
 			@Override
-			public void run() {
-				System.out.println("com.weather.AppActivity::getLocationCoordinate, runOnUiThread ");
+			public void run ()
+			{
+				System.out.println ("com.weather.AppActivity::getLocationCoordinate, runOnUiThread ");
 				app.requestLocationData();
 			}
 		});
 	}
 
-	@TargetApi(Build.VERSION_CODES.M)
-	public void requestLocationData()
+	public void requestLocationData ()
 	{
 		LocationManager locationManager = (LocationManager) app.getSystemService(Context.LOCATION_SERVICE);
-		if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+		if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+		{
 			System.out.println("com.weather.AppActivity::getLocationCoordinate, requestLocationData ");
 			requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 			return;
 		}
+
 		System.out.println("com.weather.AppActivity::getLocationCoordinate, requestLocationUpdates 2");
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) app);
 		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) app);
 	}
 
 	@Override
-	protected void onResume() {
+	protected void onResume()
+	{
 		super.onResume();
 		SDKWrapper.shared().onResume();
 	}
 
 	@Override
-	protected void onPause() {
+	protected void onPause()
+	{
 		super.onPause();
 		SDKWrapper.shared().onPause();
 	}
 
 	@Override
-	protected void onDestroy() {
+	protected void onDestroy()
+	{
 		super.onDestroy();
-		if (!isTaskRoot()) {
+		if (!isTaskRoot())
 			return;
-		}
+		
 		SDKWrapper.shared().onDestroy();
 	}
 
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
 		super.onActivityResult(requestCode, resultCode, data);
 		SDKWrapper.shared().onActivityResult(requestCode, resultCode, data);
 	}
 
 	@Override
-	protected void onNewIntent(Intent intent) {
+	protected void onNewIntent(Intent intent)
+	{
 		super.onNewIntent(intent);
 		SDKWrapper.shared().onNewIntent(intent);
 	}
 
 	@Override
-	protected void onRestart() {
+	protected void onRestart()
+	{
 		super.onRestart();
 		SDKWrapper.shared().onRestart();
 	}
 
 	@Override
-	protected void onStop() {
+	protected void onStop()
+	{
 		super.onStop();
 		SDKWrapper.shared().onStop();
 	}
 
 	@Override
-	public void onBackPressed() {
+	public void onBackPressed()
+	{
 		SDKWrapper.shared().onBackPressed();
 		super.onBackPressed();
 	}
 
 	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
+	public void onConfigurationChanged(Configuration newConfig)
+	{
 		SDKWrapper.shared().onConfigurationChanged(newConfig);
 		super.onConfigurationChanged(newConfig);
 	}
 
 	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+	protected void onRestoreInstanceState(Bundle savedInstanceState)
+	{
 		SDKWrapper.shared().onRestoreInstanceState(savedInstanceState);
 		super.onRestoreInstanceState(savedInstanceState);
 	}
 
 	@Override
-	protected void onSaveInstanceState(Bundle outState) {
+	protected void onSaveInstanceState(Bundle outState)
+	{
 		SDKWrapper.shared().onSaveInstanceState(outState);
 		super.onSaveInstanceState(outState);
 	}
 
 	@Override
-	protected void onStart() {
+	protected void onStart()
+	{
 		SDKWrapper.shared().onStart();
 		super.onStart();
 	}
 
 	@Override
-	public void onLowMemory() {
+	public void onLowMemory()
+	{
 		SDKWrapper.shared().onLowMemory();
 		super.onLowMemory();
-	}
-	
-	public static String getCurrentLocation ()
-	{
-		getLocationCoordinate ();
-		return "My City is Ho Chi Minh";
 	}
 }
